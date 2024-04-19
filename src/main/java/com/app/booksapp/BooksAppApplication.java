5989633ad438ae231283ee02370c66366e8304af
ErickTsafaf237 <ericktsafack2017@gmail.com>
2024-04-19 14:01:42 +0100

    eeeee

--- src/main/java/com/app/booksapp/BooksAppApplication.java
+++ src/main/java/com/app/booksapp/BooksAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BooksAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("nous sommes sur tchinda" );
 		SpringApplication.run(BooksAppApplication.class, args);
 	}
 
