0e231fcb46ae8fc38eb57b1e3c2057f9fb9c1e9f
TJordanP <tjordanp004@gmail.com>
2024-04-19 15:05:27 +0200

    tchindaj

--- src/main/java/com/app/booksapp/BooksAppApplication.java
+++ src/main/java/com/app/booksapp/BooksAppApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BooksAppApplication {
 
 	public static void main(String[] args) {
-		System.out.println("nous sommes sur tchinda" );
+		System.out.println("nous sommes sur tchindaj" );
 		SpringApplication.run(BooksAppApplication.class, args);
 	}
 
