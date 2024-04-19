2a3641c88a3776faf307ffc482121ccad713827a
TJordanP <tjordanp004@gmail.com>
2024-04-19 14:58:06 +0200

    A ajouter la ligne System.out.println("Spring Boot"); dans BooksAppApplication.java

--- src/main/java/com/app/booksapp/BooksAppApplication.java
+++ src/main/java/com/app/booksapp/BooksAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BooksAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Spring Boot");
 		SpringApplication.run(BooksAppApplication.class, args);
 	}
 
