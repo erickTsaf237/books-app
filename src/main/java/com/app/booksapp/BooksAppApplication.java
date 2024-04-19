0f129e0ef9410e87ee93786ee260c3c001228abb
ErickTsafaf237 <ericktsafack2017@gmail.com>
2024-04-19 14:54:51 +0100

    test master

--- src/main/java/com/app/booksapp/BooksAppApplication.java
+++ src/main/java/com/app/booksapp/BooksAppApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class BooksAppApplication {
 
 	public static void main(String[] args) {
+		System.out.println("master");
 		SpringApplication.run(BooksAppApplication.class, args);
 	}
 
